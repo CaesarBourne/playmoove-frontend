cd37ddc1c78a21f205576783be754d89a5313166
“CaesarBourne” <caesaradek@gmail.com>
2024-06-27 18:29:01 +0100

    incorporate form with username and password input

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        TEST Hello
+      <form @submit.prevent="submitForm">
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" v-model="username" id="username" required>
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" v-model="password" id="password" required>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   </template>
